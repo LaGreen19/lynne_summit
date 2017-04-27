7eaaf5886f40f72dafbdb37c9d203f55edd77ea7
LaGreen19 <lagreen227@gmail.com>
2017-04-27 11:01:12 -0400

    timer function added for footer fade out

--- wp-content/themes/divi-child/js/child-custom.js
+++ wp-content/themes/divi-child/js/child-custom.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function(){
+
 	jQuery(window).scroll(function(event) {
 	function footer()
     {
         var scroll = jQuery(window).scrollTop();
-        if(scroll > 50)
+        if(scroll > 0)
         {
             jQuery(".footer-section").fadeIn("slow").addClass("show");
         }
@@ -11,5 +12,26 @@ jQuery(document).ready(function(){
         {
             jQuery(".footer-section").fadeOut("slow").removeClass("show");
         }
+
+				jQuery('.footer-section').hover(
+					function() {
+						jQuery(this).fadeIn();
+					},
+					function(){
+						jQuery(this).fadeOut();
+}
+);
+				clearTimeout(jQuery.data(this, 'scrollTimer'));
+		         jQuery.data(this, 'scrollTimer', setTimeout(function() {
+		             if (jQuery('.footer-section').hover()) {
+		 	        	footer();
+		     		}
+		             else
+		             {
+		             	jQuery(".footer-section").fadeOut("slow");
+		             }
+		 		}, 2000));
+		     }
+		     footer();
 		 });
 });
