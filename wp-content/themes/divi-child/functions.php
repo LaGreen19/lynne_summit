bb5d74a8dcb43e21eabfd0fdcb7da53d0877282e
LaGreen19 <lagreen227@gmail.com>
2017-04-27 09:50:34 -0400

    footer show after scroll down only

--- wp-content/themes/divi-child/functions.php
+++ wp-content/themes/divi-child/functions.php
@@ -1,34 +1,22 @@
 <?php
-
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'Divi-style';
+    $parent_style = 'Divi-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'Divi', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array( 'Divi' ),
         wp_get_theme()->get('Version')
     );
 }
-
-/***ADDS DIVI BUILDER TO CUSTOM POSTS */
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-function my_et_builder_post_types( $post_types ) {
-    $post_types[] = 'speaker';
-
-    return $post_types;
+function wptuts_scripts_with_jquery()
+{
+    wp_register_script( 'child-custom', get_template_directory_uri() . '/js/child-custom.js', array( 'jquery' ) , true );
+    wp_enqueue_script( 'child-custom' );
 }
-add_filter( 'et_builder_post_types', 'my_et_builder_post_types' );
-
-// Update Dashboard CSS
-function admin_style() {
-wp_enqueue_style('admin-styles', get_template_directory_uri().'/admin.css');
-}
-add_action('admin_enqueue_scripts', 'admin_style');
-
-
-
+add_action( 'wp_enqueue_scripts', 'wptuts_scripts_with_jquery' );
 
 ?>
--- wp-content/themes/divi-child/js/child-custom.js
+++ wp-content/themes/divi-child/js/child-custom.js
@@ -1,11 +1,15 @@
-jQuery(document).ready(function() {
-	$(window).scroll(function() {
-	if($(window).scrollTop()>0)
-	{
-		$('#main-footer').fadeOut("fast").addClass("show");
-	}
-	else
-	{
-		$('#main-footer').fadeIn("fast").removeClass("show");
-	}
+jQuery(document).ready(function(){
+	jQuery(window).scroll(function(event) {
+	function footer()
+    {
+        var scroll = jQuery(window).scrollTop();
+        if(scroll > 50)
+        {
+            jQuery(".footer-section").fadeIn("slow").addClass("show");
+        }
+        else
+        {
+            jQuery(".footer-section").fadeOut("slow").removeClass("show");
+        }
+		 });
 });
